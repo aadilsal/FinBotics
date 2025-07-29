327367412b908da73cd7ab59a048a6a83c349861
aadilsal <salmanaadil52@gmail.com>
2025-07-30 01:12:36 +0500

    row 3 complete

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,15 +1,190 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
-import React from "react";
+import FlexBetween from "@/components/FlexBetween";
+import {
+  useGetKpisQuery,
+  useGetProductsQuery,
+  useGetTransactionsQuery,
+} from "@/state/api";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid, GridCellParams } from "@mui/x-data-grid";
+import React, { useMemo } from "react";
+import { Cell, Pie, PieChart } from "recharts";
 
 type Props = {};
 
 const Row3 = (props: Props) => {
+  const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[500]];
+  const { data: transactionData } = useGetTransactionsQuery();
+  const { data: productData } = useGetProductsQuery();
+  const { data: kpiData } = useGetKpisQuery();
+  // console.log(transactionData);
+  const pieChartData = useMemo(() => {
+    if (kpiData) {
+      const totalExpenses = kpiData[0].totalExpenses;
+      return Object.entries(kpiData[0].expensesByCategory).map(
+        ([key, value]) => {
+          return [
+            {
+              name: key,
+              value: value,
+            },
+            {
+              name: `${key} of Total`,
+              value: totalExpenses - value,
+            },
+          ];
+        }
+      );
+    }
+  }, [kpiData]);
+
+  const productColumns = [
+    { field: "_id", headerName: "ID", flex: 1 },
+    {
+      field: "expense",
+      headerName: "Expense",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+  ];
+
+  const transactionColumns = [
+    { field: "_id", headerName: "ID", flex: 1 },
+    {
+      field: "buyer",
+      headerName: "Buyer",
+      flex: 0.5,
+    },
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "productIds",
+      headerName: "Count",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) =>
+        (params.value as Array<string>).length,
+    },
+  ];
+
   return (
     <>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="g">
+        <BoxHeader
+          title="List of Products"
+          sideText={`${productData?.length} products`}
+        />
+        <Box
+          mt="0.5rem"
+          padding="0 0.5rem"
+          height="75%"
+          sx={{
+            "& .MuiDataGrid-root": { color: palette.grey[300], border: "none" },
+            "& .MuiDataGrid-cell": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnHeader": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden",
+            },
+          }}
+        >
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={productData || []}
+            columns={productColumns}
+          />
+        </Box>
+      </DashboardBox>
+      <DashboardBox gridArea="h">
+        <BoxHeader
+          title="Recent Orders"
+          sideText={`${transactionData?.length} Latest Transactions`}
+        />
+        <Box
+          mt="1rem"
+          padding="0 0.5rem"
+          height="80%"
+          sx={{
+            "& .MuiDataGrid-root": { color: palette.grey[300], border: "none" },
+            "& .MuiDataGrid-cell": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnHeader": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden",
+            },
+          }}
+        >
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionData || []}
+            columns={transactionColumns}
+          />
+        </Box>
+      </DashboardBox>
+      <DashboardBox gridArea="i">
+        <BoxHeader title="Expense Breakdown by Category" />
+        <FlexBetween mt="0.5rem" gap="0.5rem" p="0 1rem" textAlign="center">
+          {pieChartData?.map((data, i) => (
+            <Box key={`${data[0].name}-${i}`}>
+              <PieChart width={100} height={90}>
+                <Pie
+                  stroke="none"
+                  data={data}
+                  innerRadius={18}
+                  outerRadius={35}
+                  paddingAngle={2}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+              <Typography variant="h5">{data[0].name}</Typography>
+            </Box>
+          ))}
+        </FlexBetween>
+      </DashboardBox>
+      <DashboardBox gridArea="j">
+        <BoxHeader title="Overall Summary" />
+        <Box
+          height="15px"
+          margin="1.25rem 1rem 0.4rem 1rem"
+          bgcolor={palette.primary[800]}
+          borderRadius="1rem"
+        >
+          <Box
+            height="15px"
+            bgcolor={palette.primary[600]}
+            borderRadius="1rem"
+            width="40%"
+          ></Box>
+        </Box>
+        <Typography margin="0 1rem" variant="h6">
+          ask gpt to make it(i guess){" "}
+        </Typography>
+      </DashboardBox>
     </>
   );
 };
