78faef2826515cc817940e398a571b878bd91ef1
aadilsal <salmanaadil52@gmail.com>
2025-07-30 13:25:07 +0500

    regression done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./scenes/navbar";
 import Dashboard from "./scenes/dashboard";
+import Predictions from "./scenes/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -16,11 +17,8 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<Dashboard/>} />
-              <Route
-                path="/predictions"
-                element={<div>predictions page</div>}
-              />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>
